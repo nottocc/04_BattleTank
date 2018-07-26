d25b6f69c01998416bde03fe2e31cf09ee490b67
nottocc <chriscottonco@gmail.com>
2018-07-26 13:33:36 -0700

    projectile firing

---
+++ BattleTank/Source/BattleTank/Public/ProjectileComponent.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "ProjectileComponent.generated.h"
+
+
+//Forward Declaration
+class UProjectile;
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UProjectileComponent : public UProjectileMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
